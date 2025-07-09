9bc2ad41493a7c11b69f458cd9cae8e69c7782b6
Mark Chovava <markchovava@gmail.com>
2025-07-09 11:13:18 +0200

    Demo

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import Link from "next/link";
 
 export default function Home() {
@@ -6,7 +6,7 @@ export default function Home() {
    <>
    <section className="w-[100%] h-screen flex items-center justify-center">
     <Link href="/demo">
-      <button className="text-white hover:drop-shadow-xl p-4 rounded-xl bg-gradient-to-br transition-all ease-in-out duration-200 from-green-600 to-green-900 hover:bg-gradient-to-bl hover:from-green-600 hover:to-green-900">
+      <button className="text-white hover:drop-shadow-xl py-4 px-12 rounded-xl bg-gradient-to-br transition-all ease-in-out duration-200 from-green-600 to-green-900 hover:bg-gradient-to-bl hover:from-green-600 hover:to-green-900">
         Click to View
       </button>
     </Link>
