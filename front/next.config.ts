b5883d4db8aef90beaf95f0fcf8584175211d905
Mark Chovava <markchovava@gmail.com>
2025-07-09 11:17:51 +0200

    Demo

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   eslint:{ 
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    experimental: {
+      serverActions: {
+        bodySizeLimit: '300mb',
+      },
+    },
 };
 
 export default nextConfig;
